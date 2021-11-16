9bc218b09c35d8c9b8811352ea3569b18223316d
James Moessis <jmoessis@atlassian.com>
2021-11-17 09:46:37 +1100

    commit

--- src/main/java/com/example/demo/SomeController.java
+++ src/main/java/com/example/demo/SomeController.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import io.opentelemetry.api.OpenTelemetry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,7 @@ public class SomeController {
     @GetMapping("/")
     public String handler() {
         log.info("james is the best");
+
         return "success";
     }
 }
