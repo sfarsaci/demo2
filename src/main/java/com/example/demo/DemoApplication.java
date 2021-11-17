98c347e7f8f474645854cf66e30896d50db90d1f
Sal Farsaci <sfarsaci@atlassian.com>
2021-11-17 16:02:59 +1100

    Use client lib to init otel

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,32 @@
 package com.example.demo;
 
+import com.atlassian.obsvs.Initializer;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
+
+    @org.springframework.context.annotation.Configuration
+    public static class Configuration {
+        @PostConstruct
+        public void initObsvs() {
+            Initializer.init();
+        }
+
+        @Bean
+        public OpenTelemetry otel() {
+            return GlobalOpenTelemetry.get();
+        }
+    }
 }
